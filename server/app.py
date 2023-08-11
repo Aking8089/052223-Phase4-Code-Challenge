bb87cf7675248b65c827a5e559d35c0e85d7d0c9
Aking8089 <Princessak8918@hotmail.com>
2023-08-10 19:41:25 -0600

    Update app.py
    Still updating takes a lot slower this way even though I have several versions of my code just updating as well working on my project until I can put what I can in the read me and bring it to life and explain the situation of no vscode currently geeksquad is doing there part now they know more but will utilize my moms only if they can verify there will not be anything happening to hers as well but should have a new computer Monday if geek squad can't resolve..

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))                                                                                                                                                                                     
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
@@ -23,15 +23,64 @@ db.init_app(app)
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
-    
 
-# App test.py
-app_test.py
-importjson
-fromos importenviron
-fromflask importrequest
-fromapp importapp
-frommodels importdb, Restaurant, RestaurantPizza, Pizza
+@app.route('/restaurants') , methods = ['GET']
+def restaurants ():
+for restaurant in restaurant.query.all)
+restaurants.append (restaurants.append (restaurant. to_dict( rules" (-restaurant_pizza")))
+return make_(jsonify(restaurant_list, 200)
+
+@app.route(./restaurant'/<int.id>, methods = [GET, DELETE]
+class RestaurantById():
+def get(self, id):
+Restaurant.query.filter(Restaurant.id ==id) . first()
+
+             if Restaurant == None
+return {'error': 'Restaurant not found'}, 404
+             else:
+             if request method                                                                                                                                                                                            
+   
+        pizzas =Pizza.query.all()
+        pizza_list =[pizza.to_dict() for pizza in pizzas]
+        return_jsonify(pizza_list)
+                  else: 
+             if request method = ['GET']
+                    return make
+             
+             
+             return make_responses {"error"; Restaurant not found"}, 404
+ 
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data =request.get_json()
+if'restaurant_id'notindata or'pizza_id'notindata or'price'notindata:
+            return make_response({jsonify(('error': 'Invalid request data'), 400)
+        try:
+            price =int(data['price'])
+            ifnot(1<=price <=30):
+        return make_response(jsonify({'error': 'Price must be between $1 and $30.'}), 400)
+            restaurant_pizza =RestaurantPizza(
+                restaurant_id=data['restaurant_id'],
+                pizza_id=data['pizza_id'],
+                price=price
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            returnmake_response(jsonify({'message': 'RestaurantPizza created successfully'}), 201)
+        exceptValueError:
+            returnmake_response(jsonify({'error': 'Invalid price value. Price must be an integer.'}), 400)
+
+
+
+            
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
